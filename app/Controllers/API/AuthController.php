251005fb1c4538a1b15fce5629506a4fb47e69ce
unknown <osemensilas@gmail.com>
2025-10-18 23:04:45 +0100

    New Commit

--- app/Controllers/API/AuthController.php
+++ app/Controllers/API/AuthController.php
@@ -321,10 +321,12 @@ class AuthController{
         $data = json_decode(file_get_contents("php://input"), true);
 
         $email = $data['email'];
-        $password = $data['password'];
-        $confirmPassword = $data['confirmPassword'];
+        $password = $data['formData']['password'];
+        $confirmPassword = $data['formData']['confirmPassword'];
 
         print_r($data);
+
+        echo $password;
     }
 
     public function passResetCode(){
