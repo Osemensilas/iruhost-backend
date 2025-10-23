3cae7b416587d6798c5881922ed73b434bded398
unknown <osemensilas@gmail.com>
2025-10-23 13:49:27 +0100

    New Commit

--- app/Controllers/API/DomainRegistration.php
+++ app/Controllers/API/DomainRegistration.php
@@ -165,7 +165,7 @@ class DomainRegistration{
             return;
         }
 
-        $api = "https://resellert.enom.com/interface.asp?command=gettldlist&uid=$this->enomUserId&pw=$this->enomApiToken&responsetype=xml";
+        $api = "https://reseller.enom.com/interface.asp?command=gettldlist&uid=$this->enomUserId&pw=$this->enomApiToken&responsetype=xml";
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $api);
@@ -175,7 +175,7 @@ class DomainRegistration{
 
         //$xml = simplexml_load_string($response);
 
-        print_r($this->enomUserId);
+        print_r($response);
 
         // $data = [];
 
