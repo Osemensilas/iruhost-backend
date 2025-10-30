acc538ad3ed2ee3308219861a5075d416e819e83
unknown <osemensilas@gmail.com>
2025-10-30 23:06:51 +0100

    New Commit

--- public/test.php
+++ public/test.php
@@ -1,1 +1,26 @@
-<?php phpinfo(); ?>
+<?php 
+
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+try {
+    $mail->isSMTP();
+    $mail->Host       = 'mail.enom.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'contact@iruhost.com';
+    $mail->Password   = 'YourEmailPassword';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+
+    $mail->setFrom('contact@iruhost.com', 'IruHost');
+    $mail->addAddress('osemensilas@gmail.com');
+    $mail->Subject = 'Test';
+    $mail->Body    = 'SMTP test successful';
+
+    $mail->send();
+    echo 'Mail sent';
+} catch (\PHPMailer\PHPMailer\Exception $e) {
+    echo 'Mail failed: ' . $mail->ErrorInfo;
+}
+
