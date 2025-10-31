4fc6bff05eb2890ad718e7129770bc379ec15678
unknown <osemensilas@gmail.com>
2025-10-31 09:04:30 +0100

    New Commit

--- app/Controllers/API/AuthController.php
+++ app/Controllers/API/AuthController.php
@@ -299,7 +299,7 @@ class AuthController{
             $mail->isSMTP();
             $mail->Host = 'mail.enom.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'contact';
+            $mail->Username = 'contact@iruhost.com';
             $mail->Password = 'Bank$101Onion';
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Use constant instead of string
             $mail->Port = 465;
