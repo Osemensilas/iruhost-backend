0367f57d3714fd243bb9ac5360fc765f3f6eac6f
unknown <osemensilas@gmail.com>
2026-01-13 16:00:04 +0100

    New Coommit

--- app/Controllers/API/CallFlutter.php
+++ app/Controllers/API/CallFlutter.php
@@ -958,8 +958,7 @@ class CallFlutter {
         $whmToken = $this->cpanelApiToken;
         $cpanelServer = $this->server;
 
-        echo "$cpanelUser $whmToken $cpanelServer";
-        $url = "https://$serverIp:2087/json-api/create_user_session?api.version=1&user=$cpanelUser&service=cpaneld";
+        $url = "https://$cpanelServer:2087/json-api/create_user_session?api.version=1&user=$cpanelUser&service=cpaneld";
 
         $headers = [
             "Authorization: whm root:$whmToken"
