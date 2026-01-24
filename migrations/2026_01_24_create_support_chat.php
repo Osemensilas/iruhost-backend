143f025a5d62d0fdd4ab7b857cb6c24e1d2dfb71
unknown <osemensilas@gmail.com>
2026-01-24 13:19:15 +0100

    New Commit

--- migrations/2026_01_24_create_support_chat.php
+++ migrations/2026_01_24_create_support_chat.php
@@ -2,14 +2,13 @@
 
 return function ($pdo){
     $pdo->exec("
-        CREATE TABLE IF NOT EXISTS support_chats (
+        CREATE TABLE IF NOT EXISTS chats (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            user_id VARCHAR(36) NOT NULL,
-            reciever_id VARCHAR(36) NOT NULL,
             ticket_id VARCHAR(36) NOT NULL,
-            user_msg VARCHAR(1000) NOT NULL,
-            reciever_msg VARCHAR(1000) NOT NULL,
-            image VARCHAR(255) DEFAULT NULL,
+            user_id VARCHAR(36) NOT NULL,
+            reciever_id VARCHAR(255) NOT NULL,
+            message VARCHAR(300) NOT NULL,
+            image VARCHAR(255) NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
     ");
