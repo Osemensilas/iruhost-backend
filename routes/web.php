cf48c546b3a6d7c4580ca1aaf177405269d453f0
unknown <osemensilas@gmail.com>
2026-01-24 14:06:26 +0100

    New Commit

--- routes/web.php
+++ routes/web.php
@@ -70,6 +70,7 @@ $router->post('/api/send-ticket', [ChatsController::class, 'openTicket']);
 $router->get('/api/user-tickets', [ChatsController::class, 'getTickets']);
 $router->get('/api/unresolved-tickets', [ChatsController::class, 'getUnresolvedTickets']);
 $router->post('/api/post-support-message', [ChatsController::class, 'postSupportReply']);
+$router->get('/api/get-support-chats', [ChatsController::class, 'getSupportMessages']);
 $router->get('/api/get-dashboard', [UserProducts::class, 'getDashboardProducts']);
 $router->post('/api/get-user-domain-products', [UserProducts::class, 'getDomainProducts']);
 $router->get('/api/get-expiring', [UserProducts::class, 'expiringProduct']);
