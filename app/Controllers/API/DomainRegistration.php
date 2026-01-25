050cba16fa6f2b6813fa47afd5fb88fd426203ea
unknown <osemensilas@gmail.com>
2026-01-25 13:41:06 +0100

    New Commit

--- app/Controllers/API/DomainRegistration.php
+++ app/Controllers/API/DomainRegistration.php
@@ -166,7 +166,7 @@ class DomainRegistration{
         ];
         $headers = [
             "username: osemensilas@gmail.com",
-            "token: " . base64_encode(hash_hmac("sha256", "sKUcg0MeTqQyVvySlVcuk6Erx1G84Al5", "osemensilas@example.com:" . gmdate("y-m-d H")))
+            "token: " . base64_encode(hash_hmac("sha256", "sKUcg0MeTqQyVvySlVcuk6Erx1G84Al5", "osemensilas@gmail.com:" . gmdate("y-m-d H")))
         ];
 
         $curl = curl_init();
