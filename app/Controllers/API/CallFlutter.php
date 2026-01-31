eab0316f22df59f25065065df4751a1fc1320a46
unknown <osemensilas@gmail.com>
2026-01-31 10:19:40 +0100

    New COmmit

--- app/Controllers/API/CallFlutter.php
+++ app/Controllers/API/CallFlutter.php
@@ -65,7 +65,8 @@ class CallFlutter {
         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         //https://www.iruhost.com/checkout?status=successful&tx_ref=ref_697dbdd689438&transaction_id=1971804486
-
+        print_r($rows);
+        
         if ($stmt->rowCount() > 1){
             
             foreach($rows as $row){
