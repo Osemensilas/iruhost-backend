f8983719f976ec5f676ca1139e4c8c7304b5d43d
unknown <osemensilas@gmail.com>
2026-01-31 08:11:29 +0100

    New Commit

--- app/Controllers/API/DomainRegistration.php
+++ app/Controllers/API/DomainRegistration.php
@@ -336,6 +336,37 @@ class DomainRegistration{
         $tdl = substr($domainName, strpos($domainName, '.') + 1);
         $sld = substr($domainName, 0, strpos($domainName, '.'));
 
+        $tlds = ['ng', 'com.ng', 'org.ng', 'gov.ng', 'edu.ng', 'net.ng', 'sch.ng', 'name.ng', 'mobi.ng', 'mil.ng', 'i.ng'];
+
+        if (in_array($tdl, $tlds)){
+            $endpoint = "https://www.whogohost.com/host/modules/addons/DomainsReseller/api/index.php";
+            $action = "/domains/$domainName/nameservers";
+            $params = [
+                "domain"    => $domainName,
+            ];
+
+            $headers = [
+                "username: " . $this->whogohostUsername,
+                "token: " . base64_encode(hash_hmac("sha256", "$this->whogohostApi", "$this->whogohostUsername:".gmdate("y-m-d H"))),
+            ];
+
+            $curl = curl_init();
+
+            curl_setopt($curl, CURLOPT_URL, "{$endpoint}{$action}");
+            curl_setopt($curl, CURLOPT_POST, true);  // Note: This should be changed to curl_setopt($curl, CURLOPT_HTTPGET, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));  // Note: This line is typically unnecessary for a GET request
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+            $response = curl_exec($curl);
+            curl_close($curl);
+
+            print_r($response);
+            
+            return;
+        }
+
         $url = "https://reseller.enom.com/interface.asp?command=ModifyNS&uid=$this->enomUserId&pw=$this->enomApiToken&sld=$sld&tld=$tdl&ns1=$dns1&ns2=$dns2&ns3=$dns3&ns4=$dns4&responsetype=xml";
     
         $ch = curl_init();
@@ -413,37 +444,6 @@ class DomainRegistration{
 
         $tdl = substr($domainName, strpos($domainName, '.') + 1);
         $sld = substr($domainName, 0, strpos($domainName, '.'));
-
-        $tlds = ['ng', 'com.ng', 'org.ng', 'gov.ng', 'edu.ng', 'net.ng', 'sch.ng', 'name.ng', 'mobi.ng', 'mil.ng', 'i.ng'];
-
-        if (in_array($tdl, $tlds)){
-            $endpoint = "https://www.whogohost.com/host/modules/addons/DomainsReseller/api/index.php";
-            $action = "/domains/$domainName/nameservers";
-            $params = [
-                "domain"    => $domainName,
-            ];
-
-            $headers = [
-                "username: " . $this->whogohostUsername,
-                "token: " . base64_encode(hash_hmac("sha256", "$this->whogohostApi", "$this->whogohostUsername:".gmdate("y-m-d H"))),
-            ];
-
-            $curl = curl_init();
-
-            curl_setopt($curl, CURLOPT_URL, "{$endpoint}{$action}");
-            curl_setopt($curl, CURLOPT_POST, true);  // Note: This should be changed to curl_setopt($curl, CURLOPT_HTTPGET, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));  // Note: This line is typically unnecessary for a GET request
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-            $response = curl_exec($curl);
-            curl_close($curl);
-
-            print_r($response);
-            
-            return;
-        }
 
         $url = "https://reseller.enom.com/interface.asp?command=GetHosts&uid=$this->enomUserId&pw=$this->enomApiToken&SLD=$sld&TLD=$tdl&ResponseType=XML";
 
