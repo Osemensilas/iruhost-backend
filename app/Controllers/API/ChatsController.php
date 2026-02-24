9d9d0edf5b5ea31fcece479652f0eba483caf090
unknown <osemensilas@gmail.com>
2026-02-24 15:45:35 +0100

    New Commit

--- app/Controllers/API/ChatsController.php
+++ app/Controllers/API/ChatsController.php
@@ -611,7 +611,7 @@ class ChatsController{
             return;
         }
 
-        $data = json_decode(file_get_contents("php://input"), true);
+        $data = $_POST['message'] ?? '';
 
         print_r($data);
 
