d5cb7cde7bd55b4db22fea1dcd6d4678381eea60
unknown <osemensilas@gmail.com>
2026-08-07 22:00:12 +0100

    New Commit

--- app/Controllers/API/AddToCart.php
+++ app/Controllers/API/AddToCart.php
@@ -283,8 +283,8 @@ class AddToCart {
         $emailProduct = 'Email Registration';
         $emailBilling = 'year';
         $emailDomain = "";
-        $emailPrice = $data['amount'] ?? null;
-        $emailRenew = $data['amount'] ?? null;
+        $emailPrice = $data['price'] ?? null;
+        $emailRenew = $data['price'] ?? null;
 
         echo json_encode([
             "package" => $emailName,
