2a84fd3e20ea6f8fd49a433bccebbf4c83646c06
unknown <osemensilas@gmail.com>
2026-08-08 17:26:41 +0100

    New Commit

---
+++ migrations/2026_08_08_create_iruap_mail.php
@@ -0,0 +1,16 @@
+<?php
+
+return function ($pdo){
+    $pdo->exec("
+        CREATE TABLE IF NOT EXISTS iruap_professional_mails (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id VARCHAR(36) NOT NULL,
+            product_id VARCHAR(36) NOT NULL,
+            email_id VARCHAR(36) NOT NULL,
+            mailbox VARCHAR(50) NOT NULL,
+            domain VARCHAR(50) NOT NULL,
+            password VARCHAR(50) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    ");
+};
